098eec19e37021fd7eaf0295b22c4c8b9cd568ed
Biniyambd <biniyambd@gmail.com>
2023-01-29 16:05:14 -0700

    add a class

---
+++ ride/src/main/java/ride/rider.java
@@ -0,0 +1,15 @@
+package ride;
+
+public class rider {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private int dID;
+		private char fname;
+		private char lname;
+		private int carID;
+		
+
+	}
+
+}
